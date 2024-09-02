99c5f601c26534facdac5f04dba25fdf8e5270cd
nikhilrawat-98 <157377389+nikhilrawat-98@users.noreply.github.com>
2024-09-02 14:29:12 +0100

    app.py

--- app.py
+++ app.py
@@ -237,7 +237,7 @@ def fetch_articles(urls, headers):
 
 # Function to encode an image to base64
 def get_img_as_base64(file):
-    with open(file, "rb") as f):
+    with open(file, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
